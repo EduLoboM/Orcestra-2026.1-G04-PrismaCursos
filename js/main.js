cb9a4470f130e17518e3c794a71257f33c2f0a6b
VitorRoss1 <vitorcrossi159@gmail.com>
2026-05-30 19:40:27 -0300

    feat(gamificacao desconto):implementa logica do desconto progressivo

--- js/main.js
+++ js/main.js
@@ -18,20 +18,60 @@ document.querySelector("#nova-secao").addEventListener("click", () => {
 */
 
 
-// 1. Pegamos o botão no HTML pela id
-const btnEnviar = document.getElementById('btn-enviar');
+// ------ GAMIFICAÇÃO DESCONTO PROGESSIVO ------
+let descontoAtual = 5; 
 
-// 2. lista de cursos válidos 
+//flags: evitar o desconto infinito
+let clicouCursos = false;
+let clicouSobre = false;
+let clicouContato = false;
+
+//FUNÇÃO QUE ATUALIZA O VISUAL
+function atualizarVisualDoDesconto() {
+  const fundoTriangulo = document.getElementById('selo-desconto');
+  const textoPorcentagem = document.getElementById('valor-desconto');
+
+  textoPorcentagem.innerText = descontoAtual + "%";  
+  fundoTriangulo.className = "triangulo-conteudo desconto-" + descontoAtual; // Atualiza a cor e reescreve a classe. se desconto for 15, a string vira "triangulo-conteudo desconto-15"
+}
+
+// Cliques em Cursos,Sobre,Contato:
+document.getElementById('link-cursos').addEventListener('click', function() { 
+  if (clicouCursos === false) { //só adiciona 1vez
+    descontoAtual += 5; 
+    clicouCursos = true;               
+    atualizarVisualDoDesconto();   // Roda a função que muda a tela
+  }
+});
+
+document.getElementById('link-sobre').addEventListener('click', function() {
+  if (clicouSobre === false) {
+    descontoAtual += 5; 
+    clicouSobre = true;
+    atualizarVisualDoDesconto();
+  }
+});
+
+document.getElementById('link-contato').addEventListener('click', function() {
+  if (clicouContato === false) {
+    descontoAtual += 5;
+    clicouContato = true;
+    atualizarVisualDoDesconto();
+  }
+});
+
+
+// --------- VERIFICACOES DE INPUT ---------
+const btnEnviar = document.getElementById('btn-enviar'); //Pegamos o botão no HTML pela id
+
+//lista de cursos válidos 
 const cursosValidos = ['devops','gestão de projetos','ui/ux', 'design 3d',
   'gestão pessoal','data analytics','motion design','design gráfico',
   'desenvolvimento full stack','cybersecurity','gestão de projetos',
   'marketing digital','liderança e gestão de pessoas']; 
 
-// 3. Adicionamos o "escutador" de cliques no botão (func ao clicar)
-btnEnviar.addEventListener('click', function(evento) { //evento é um relatorio de clique
-  
-  // impede que o <a>(link) te leve para a outra página automaticamente
-  evento.preventDefault(); 
+  btnEnviar.addEventListener('click', function(evento) { //evento é um objeto do js q guarda informacoes da açao(no caso click),do elemento q sofreu a ação...
+  evento.preventDefault(); // impede que o <a>(link) te leve para a outra página automaticamente
 
   // Pegamos os valores que o usuário digitou e tiramos os espaços em branco nas pontas (trim)
   const nome = document.getElementById('input-nome').value.trim();
@@ -40,10 +80,9 @@ btnEnviar.addEventListener('click', function(evento) { //evento é um relatorio 
   const curso = document.getElementById('input-curso').value.trim().toLowerCase(); // Transforma em minúscula
   const assunto = document.getElementById('input-assunto').value.trim();
 
-  // Array para guardar erros
-  let erros = [];
+  let erros = []; // Array para guardar erros
 
-  // --- VALIDAÇÕES ---
+  // VALIDAÇÕES
   // 1. Checa se tem algum campo vazio
   if (!nome || !email || !celular || !curso || !assunto) {
     erros.push("Por favor, preencha todos os campos do formulário.");
@@ -66,13 +105,13 @@ btnEnviar.addEventListener('click', function(evento) { //evento é um relatorio 
     erros.push("Curso não encontrado. Verifique se digitou corretamente.");
   }
 
-  // --- RESULTADO FINAL ---
+  //RESULTADO FINAL
   if (erros.length > 0) { //exibe os alertas e NÃO muda de página
     alert(erros.join('\n\n')); // Junta todos os erros com uma quebra de linha e exibe
   } 
   else {
-    const paginaDestino = btnEnviar.getAttribute('href'); //extrai o link no href do btn
-    window.location.href = paginaDestino; //muda de pág
+    const paginaDestino = btnEnviar.getAttribute('href'); //extrai o link no href do btnEnviar
+    window.location.href = paginaDestino; //mudamos de pág manualmente
   }
 } //funcao no parametro
-); //
+);
